e26ba06ab7636476ae9837318a4c8408d1328cdc
unknown <sushant.shekhar151997@gmail.com>
2023-08-18 00:47:50 +0530

    minnor bug remove

--- model/purchasehistory.js
+++ model/purchasehistory.js
@@ -6,7 +6,7 @@ const purchaseHistorySchema = new mongoose.Schema({
   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   productName:{type:String,required:true},
   quantity: { type: Number, required: true },
-  purchaseDate: { type: Date, default: Date.now },
+  purchaseDate: { type: Date, default: Date.now() },
   sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
